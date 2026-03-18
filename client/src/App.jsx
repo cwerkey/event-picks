bda9fb4dafdc4118278c8f9a8c261d617e6f706f
cwerkey <130715356+cwerkey@users.noreply.github.com>
2026-03-18 09:31:03 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,50 @@
 
+import { useState } from "react";
+
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const res = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ username, password })
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    if (data.token) {
+      alert("Login success");
+      localStorage.setItem("token", data.token);
+    } else {
+      alert("Login failed");
+    }
+  };
+
   return (
-    <div>
-      <h1>Event Picks Running</h1>
+    <div style={{ padding: 40, color: "white" }}>
+      <h1>Event Picks Login</h1>
+
+      <input
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        style={{ display: "block", marginBottom: 10 }}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        style={{ display: "block", marginBottom: 10 }}
+      />
+
+      <button onClick={login}>Login</button>
     </div>
   );
 }
